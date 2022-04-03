7439b460ce33d96cad1c0876d88595a8ac81d6c2
jhonywalkeer <jhonyw74@gmail.com>
2022-04-02 23:25:34 -0300

    feat: add dto's

--- backend/src/app/todo/todo.controller.ts
+++ backend/src/app/todo/todo.controller.ts
@@ -10,6 +10,8 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
+import { CreateTodoDto } from './dto/create-todo.dto';
+import { UpdateTodoDto } from './dto/update-todo.dto';
 import { TodoService } from './todo.service';
 
 @Controller('api/v1/todos')
@@ -22,7 +24,7 @@ export class TodoController {
   }
 
   @Post()
-  async create(@Body() body) {
+  async create(@Body() body: CreateTodoDto) {
     return await this.todoService.create(body);
   }
 
@@ -32,7 +34,7 @@ export class TodoController {
   }
 
   @Put(':id')
-  async update(@Param('id', new ParseUUIDPipe()) id: string, @Body() body) {
+  async update(@Param('id', new ParseUUIDPipe()) id: string, @Body() body: UpdateTodoDto) {
     return await this.todoService.update(id, body);
   }
 
